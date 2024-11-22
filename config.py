d0906eea2649372816f35959366d082a62105e8a
anjanaganesh06 <141896402+anjanaganesh06@users.noreply.github.com>
2024-11-22 17:13:10 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    MYSQL_HOST = 'localhost'  # Change to your MySQL host
+    MYSQL_USER = 'root'  # Change to your MySQL username
+    MYSQL_PASSWORD = 'Grape2004'  # Change to your MySQL password
+    MYSQL_DB = 'dbmsminiproj'
